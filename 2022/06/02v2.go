5172c7edfea450826ba58d1b70ce36f0bcc881fe
Dylan Butler <dylanbutler@outlook.com.au>
2022-12-07 16:55:05 +1100

    2022 day 7 + day 6v2

---
+++ 2022/06/02v2.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+)
+
+//go:embed input.txt
+var input string
+
+//go:embed test.txt
+var test string
+
+func main() {
+	fmt.Println("Test")
+	fmt.Println(do(test))
+	fmt.Println()
+	fmt.Println("Solution")
+	fmt.Println(do(input))
+}
+
+func do(s string) int {
+	return solve(s)
+}
+
+func solve(s string) int {
+	l := 14
+	for i := 0; i < len(s)-l; i++ {
+		m := make(map[rune]bool)
+		for _, char := range s[i : i+l] {
+			m[char] = true
+		}
+		if len(m) == l {
+			return i + l
+		}
+	}
+	return -1
+}
---
+++ 2022/07/01.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"strings"
+
+	"github.com/dbut2/advent-of-code/utils"
+)
+
+//go:embed input.txt
+var input string
+
+//go:embed test.txt
+var test string
+
+func main() {
+	fmt.Println("Test")
+	fmt.Println(do(test))
+	fmt.Println()
+	fmt.Println("Solution")
+	fmt.Println(do(input))
+}
+
+func do(s string) int {
+	strs := strings.Split(strings.Trim(s, "$ "), "\n$ ")
+	return solve(strs)
+}
+
+func solve(s []string) int {
+	root := &Tree{
+		Name: "/",
+	}
+	root.Root = root
+
+	pointer := root
+
+	for _, str := range s {
+		lines := strings.Split(str, "\n")
+		firstLine := strings.Split(lines[0], " ")
+
+		cmd := firstLine[0]
+
+		switch cmd {
+		case "cd":
+			pointer = pointer.MoveTo(firstLine[1])
+		case "ls":
+			for _, file := range lines[1:] {
+				line := strings.Split(file, " ")
+
+				size := 0
+				ft := Dir
+				if line[0] != "dir" {
+					size = utils.Sti(line[0])
+					ft = File
+				}
+
+				pointer.AddChild(line[1], size, ft)
+			}
+		}
+	}
+
+	pointer = pointer.MoveTo("/")
+
+	return pointer.TotalOfChildDirectoriesLessThanOrEqualTo(100000)
+}
+
+type Tree struct {
+	Name     string
+	Type     Type
+	Root     *Tree
+	Parent   *Tree
+	Children []*Tree
+	Size     int
+}
+
+type Type int
+
+const (
+	Unknown Type = iota
+	Dir
+	File
+)
+
+func (t Tree) TotalOfChildDirectoriesLessThanOrEqualTo(size int) int {
+	if t.Type == File {
+		return 0
+	}
+
+	total := 0
+	if t.SizeOf() <= size {
+		total += t.SizeOf()
+	}
+	for _, child := range t.Children {
+		total += child.TotalOfChildDirectoriesLessThanOrEqualTo(size)
+	}
+	return total
+}
+
+func (t *Tree) MoveUp() *Tree {
+	return t.Parent
+}
+
+func (t *Tree) MoveDown(dir string) *Tree {
+	for _, child := range t.Children {
+		if child.Name == dir {
+			return child
+		}
+	}
+	child := &Tree{
+		Name:   dir,
+		Type:   Dir,
+		Root:   t.Root,
+		Parent: t,
+	}
+	t.Children = append(t.Children, child)
+	return child
+}
+
+func (t *Tree) AddChild(name string, size int, ft Type) {
+	child := &Tree{
+		Name:   name,
+		Type:   ft,
+		Size:   size,
+		Root:   t.Root,
+		Parent: t,
+	}
+	t.Children = append(t.Children, child)
+}
+
+func (t *Tree) SizeOf() int {
+	size := t.Size
+	for _, child := range t.Children {
+		size += child.SizeOf()
+	}
+	return size
+}
+
+func (t *Tree) MoveTo(dir string) *Tree {
+	if dir == "" {
+		return t
+	}
+
+	path := strings.Split(dir, "/")
+
+	current := path[0]
+	next := strings.Join(path[1:], "/")
+
+	switch current {
+	case ".":
+		return t.MoveTo(next)
+	case "..":
+		return t.MoveUp().MoveTo(next)
+	case "":
+		return t.Root.MoveTo(next)
+	default:
+		return t.MoveDown(current).MoveTo(next)
+	}
+}
