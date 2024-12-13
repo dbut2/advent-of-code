934160d872d22f9202abf458536afca36db91f43
Dylan Butler <dylan@but.la>
2024-12-13 21:20:33 +1100

    2024 day 13 sped

--- 2024/13/1.go
+++ 2024/13/1.go
@@ -1,34 +1,24 @@
 package main
 
 import (
-	"math"
-
 	"github.com/dbut2/advent-of-code/pkg/harness"
-	"github.com/dbut2/advent-of-code/pkg/strings"
 )
 
-func solve(input [][]string) int {
+func solve(input [][]int) int {
 	tokens := 0
-	for _, machine := range input {
-		var a, b, p [2]int
 
-		for i, line := range machine {
-			nums := strings.Ints(line)
-			switch i {
-			case 0:
-				a[0], a[1] = nums[0], nums[1]
-			case 1:
-				b[0], b[1] = nums[0], nums[1]
-			case 2:
-				p[0], p[1] = nums[0], nums[1]
-			}
-		}
+	for i := range len(input) / 4 {
+		a0, a1 := input[4*i][0], input[4*i][1]
+		b0, b1 := input[4*i+1][0], input[4*i+1][1]
+		p0, p1 := input[4*i+2][0], input[4*i+2][1]
 
-		an := float64(b[0]*p[1]-p[0]*b[1]) / float64(b[0]*a[1]-a[0]*b[1])
-		bn := float64(a[0]*p[1]-p[0]*a[1]) / float64(a[0]*b[1]-b[0]*a[1])
+		aNum := b0*p1 - p0*b1
+		aDen := b0*a1 - a0*b1
+		bNum := a0*p1 - p0*a1
+		bDem := a0*b1 - b0*a1
 
-		if math.Mod(an, 1) == 0 && math.Mod(bn, 1) == 0 {
-			tokens += int(an)*3 + int(bn)
+		if aNum%aDen == 0 && bNum%bDem == 0 {
+			tokens += 3*aNum/aDen + bNum/bDem
 		}
 	}
 
