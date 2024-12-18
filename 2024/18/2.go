519138d53bb12a11c5e9887d4325a6eca403edd9
Dylan Butler <dylan@but.la>
2024-12-18 11:02:40 +0000

    2024 day 18 sped
    6347 TRIALS IN 1.000000151s
    MIN: 128.458µs
    MED: 143.235µs
    MAX: 506.615µs

--- 2024/18/2.go
+++ 2024/18/2.go
@@ -1,25 +1,114 @@
 package main
 
 import (
-	"slices"
+	"time"
 
+	"github.com/dbut2/advent-of-code/pkg/benchmark"
 	"github.com/dbut2/advent-of-code/pkg/harness"
-	"github.com/dbut2/advent-of-code/pkg/space"
 )
 
 func solve(input [][]int) [2]int {
-	g := space.NewGrid[bool](71, 71)
-	for b := 0; ; b++ {
-		g.Set(space.Cell{input[b][0], input[b][1]}, true)
-		for _, flood := range space.FloodAll(g) {
-			if slices.Contains(flood, space.Cell{0, 0}) && !slices.Contains(flood, space.Cell{70, 70}) {
-				return [2]int(input[b])
+	grid := [71][71]bool{}
+	i, j, k := 0, len(input)/2, len(input)
+	lastJ := 0
+	for i < k-1 {
+		if lastJ < j {
+			for _, cell := range input[lastJ:j] {
+				grid[cell[0]][cell[1]] = true
+			}
+		} else {
+			for _, cell := range input[j:lastJ] {
+				grid[cell[0]][cell[1]] = false
 			}
 		}
+		lastJ = j
+		state := grid
+		stack := [][2]int{{0, 0}}
+		end := false
+		for len(stack) > 0 {
+			cell := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			if state[cell[0]][cell[1]] {
+				continue
+			}
+			state[cell[0]][cell[1]] = true
+			{
+				next := cell
+				next[0]++
+				if next[0] >= len(grid) {
+					goto a
+				}
+				if grid[next[0]][next[1]] {
+					goto a
+				}
+				if next == [2]int{70, 70} {
+					end = true
+					break
+				}
+				stack = append(stack, next)
+			}
+		a:
+			{
+				next := cell
+				next[1]++
+				if next[1] >= len(grid[0]) {
+					goto c
+				}
+				if grid[next[0]][next[1]] {
+					goto c
+				}
+				if next == [2]int{70, 70} {
+					end = true
+					break
+				}
+				stack = append(stack, next)
+			}
+		c:
+			{
+				next := cell
+				next[0]--
+				if next[0] < 0 {
+					goto b
+				}
+				if grid[next[0]][next[1]] {
+					goto b
+				}
+				if next == [2]int{70, 70} {
+					end = true
+					break
+				}
+				stack = append(stack, next)
+			}
+		b:
+			{
+				next := cell
+				next[1]--
+				if next[1] < 0 {
+					goto d
+				}
+				if grid[next[0]][next[1]] {
+					goto d
+				}
+				if next == [2]int{70, 70} {
+					end = true
+					break
+				}
+				stack = append(stack, next)
+			}
+		d:
+		}
+		if end {
+			i = j
+		} else {
+			k = j
+		}
+		j = i + (k-i)/2
 	}
+	return [2]int(input[j])
 }
 
 func main() {
 	h := harness.New(solve)
+	h.Benchmark(benchmark.Time(time.Second))
 	h.Run()
 }
