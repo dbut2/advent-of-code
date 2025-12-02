dc8cef5100f0b079d0cbf830997a464a91eac236
Dylan Butler <dylan@but.la>
2025-12-03 08:25:04 +1100

    2025 day 2: improved

--- 2025/02/1.go
+++ 2025/02/1.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
 	"strings"
 
-	"github.com/dbut2/advent-of-code/pkg/benchmark"
 	"github.com/dbut2/advent-of-code/pkg/harness"
 )
 
@@ -15,23 +12,23 @@ func solve(input string) int {
 
 	for _, line := range lines {
 		parts := strings.Split(line, "-")
+		start, end := atoi(parts[0]), atoi(parts[1])
 
-		start, _ := strconv.Atoi(parts[0])
-		end, _ := strconv.Atoi(parts[1])
+		// split ranges into a list of ranges where each range has a fixed
+		// number of digits
+		for _, r := range splitRanges(start, end) {
+			start, end := r[0], r[1]
 
-		// test every number in range inclusively
-		for i := start; i <= end; i++ {
-			num := fmt.Sprint(i)
-
-			l := len(num)
-			// skip odd length numbers
-			if l%2 != 0 {
+			d := divisors[log10(start)]
+			if d == 0 {
 				continue
 			}
 
-			// add as invalid if first and second half are the same
-			if num[:l/2] == num[l/2:] {
-				total += i
+			// check all divisible numbers of divisor between start and end
+			check := ((start + d - 1) / d) * d
+			for check <= end {
+				total += check
+				check += d
 			}
 		}
 	}
@@ -39,9 +36,59 @@ func solve(input string) int {
 	return total
 }
 
+var divisors = []int{
+	2:  11,
+	4:  101,
+	6:  1001,
+	8:  10001,
+	10: 100001,
+}
+
+// splitRanges will return a list of ranges grouped by length of numbers
+// eg [3,300] => [[3,9],[10,99],[100,300]]
+func splitRanges(start, end int) [][]int {
+	a, b := log10(start), log10(end)
+	if a == b {
+		return [][]int{{start, end}}
+	}
+
+	ranges := [][]int{{start, pow10(a) - 1}}
+	for i := a; i < b-1; i++ {
+		ranges = append(ranges, []int{pow10(i), pow10(i+1) - 1})
+	}
+	ranges = append(ranges, []int{pow10(b - 1), end})
+
+	return ranges
+}
+
+// digits in number
+func log10(n int) int {
+	for i := range pows10 {
+		if n < pows10[i] {
+			return i
+		}
+	}
+	panic("too big")
+}
+
+// 10 ** n
+func pow10(n int) int {
+	return pows10[n]
+}
+
+var pows10 = [11]int{1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000, 10000000000}
+
+func atoi(s string) int {
+	n := 0
+	for i := range s {
+		n *= 10
+		n += int(s[i] - '0')
+	}
+	return n
+}
+
 func main() {
 	h := harness.New(solve)
-	h.Benchmark(benchmark.Count(100))
 	h.Expect(1, 1227775554)
 	h.Run()
 }
--- 2025/02/2.go
+++ 2025/02/2.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
 	"strings"
 
-	"github.com/dbut2/advent-of-code/pkg/benchmark"
 	"github.com/dbut2/advent-of-code/pkg/harness"
 )
 
@@ -15,32 +12,23 @@ func solve(input string) int {
 
 	for _, line := range lines {
 		parts := strings.Split(line, "-")
+		start, end := atoi(parts[0]), atoi(parts[1])
 
-		start, _ := strconv.Atoi(parts[0])
-		end, _ := strconv.Atoi(parts[1])
+		// split ranges into a list of ranges where each range has a fixed
+		// number of digits
+		for _, r := range splitRanges(start, end) {
+			seen := map[int]bool{}
+			start, end := r[0], r[1]
 
-		// test every number in range inclusively
-		for i := start; i <= end; i++ {
-			num := fmt.Sprint(i)
-			l := len(num)
-
-			// split number into n pieces for every factor of the length
-			for j := 2; j <= l; j++ {
-				if l%j != 0 {
-					continue
-				}
-
-				// if all pieces other than the first match with the first,
-				// the whole number must be a repeating sequence
-				all := true
-				for k := 1; k < j; k++ {
-					if line[:l/j] != line[k*(l/j):(k+1)*(l/j)] {
-						all = false
+			// for each divisor, find all unique divisible numbers in the range
+			for _, d := range divisors[log10(start)] {
+				check := ((start + d - 1) / d) * d
+				for check <= end {
+					if !seen[check] {
+						total += check
+						seen[check] = true
 					}
-				}
-				if all {
-					total += i
-					break
+					check += d
 				}
 			}
 		}
@@ -49,9 +37,63 @@ func solve(input string) int {
 	return total
 }
 
+var divisors = [][]int{
+	2:  {11},
+	3:  {111},
+	4:  {101},
+	5:  {11111},
+	6:  {1001, 10101},
+	7:  {1111111},
+	8:  {10001},
+	9:  {1001001},
+	10: {100001, 101010101},
+}
+
+// splitRanges will return a list of ranges grouped by length of numbers
+// eg [3,300] => [[3,9],[10,99],[100,300]]
+func splitRanges(start, end int) [][]int {
+	a, b := log10(start), log10(end)
+	if a == b {
+		return [][]int{{start, end}}
+	}
+
+	ranges := [][]int{{start, pow10(a) - 1}}
+	for i := a; i < b-1; i++ {
+		ranges = append(ranges, []int{pow10(i), pow10(i+1) - 1})
+	}
+	ranges = append(ranges, []int{pow10(b - 1), end})
+
+	return ranges
+}
+
+// digits in number
+func log10(n int) int {
+	for i := range pows10 {
+		if n < pows10[i] {
+			return i
+		}
+	}
+	panic("too big")
+}
+
+// 10 ** n
+func pow10(n int) int {
+	return pows10[n]
+}
+
+var pows10 = [11]int{1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000, 10000000000}
+
+func atoi(s string) int {
+	n := 0
+	for i := range s {
+		n *= 10
+		n += int(s[i] - '0')
+	}
+	return n
+}
+
 func main() {
 	h := harness.New(solve)
-	h.Benchmark(benchmark.Count(30))
 	h.Expect(1, 4174379265)
 	h.Run()
 }
