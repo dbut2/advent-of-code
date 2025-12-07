2b1e7ab1b8001b74119f663d7a68d0f367586b6b
Dylan Butler <dylan@but.la>
2025-12-07 21:52:30 +1100

    2025 day 7: refactor

--- 2025/07/2.go
+++ 2025/07/2.go
@@ -5,31 +5,31 @@ import (
 )
 
 func solve(input []string) int {
-	prevLine := make([]int, len(input[0]))
-	for x, v := range input[0] {
-		if v == 'S' {
-			prevLine[x] = 1
+	prevLineCount := make([]int, len(input[0]))
+	for i, char := range input[0] {
+		if char == 'S' {
+			prevLineCount[i] = 1
 		}
 	}
 
 	for _, line := range input {
 		lineCount := make([]int, len(line))
-		for j, char := range line {
+		for i, char := range line {
 			if char == '^' {
-				lineCount[j-1] += prevLine[j]
-				lineCount[j+1] += prevLine[j]
+				lineCount[i-1] += prevLineCount[i]
+				lineCount[i+1] += prevLineCount[i]
 			} else {
-				lineCount[j] += prevLine[j]
+				lineCount[i] += prevLineCount[i]
 			}
 		}
-		prevLine = lineCount
+		prevLineCount = lineCount
 	}
 
-	count := 0
-	for _, v := range prevLine {
-		count += v
+	total := 0
+	for _, count := range prevLineCount {
+		total += count
 	}
-	return count
+	return total
 }
 
 func main() {
